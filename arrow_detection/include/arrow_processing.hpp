f536fd9f8cd0b7ce5707fb2a639a6e43431b1664
daiemna <daiem.dna@gmail.com>
2015-01-12 02:05:11 +0100

    removed my implementaion of otsu's

--- arrow_detection/include/arrow_processing.hpp
+++ arrow_detection/include/arrow_processing.hpp
@@ -16,7 +16,6 @@ namespace arrow_detection {
 	int preProcessing(Mat frame,Mat& pro_image,double thresh=-1,Size ksize=Size(51,51));
 	int segmentation(Mat binary_image,vector<Rect> &objects, vector <vector<Point2i>> &blobs);
 	int featureExteraction(Mat arrow,vector<float> &features);
-	/*int otsuThresholding(Mat frame,float& thresh);*/
 	int findBlobs(const Mat &binary, vector <vector<Point2i>> &blobs,int min_pixel_count=9000);
 } // arrow_detection
 
